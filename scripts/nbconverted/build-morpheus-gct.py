d64b538330c6fe9073313f6cde454958e6364e12
gwaygenomics <gregory.way@gmail.com>
2019-08-12 10:52:50 -0400

    add batch 2 to morpheus output

--- scripts/nbconverted/build-morpheus-gct.py
+++ scripts/nbconverted/build-morpheus-gct.py
@@ -20,6 +20,8 @@ import pandas as pd
 get_ipython().run_line_magic('load_ext', 'rpy2.ipython')
 
 
+# ## Batch 1
+
 # In[3]:
 
 
@@ -95,4 +97,37 @@ cp_non15_collapsed_df = cp_non15_df.groupby(replicate_cols).mean().reset_index()
 
 
 get_ipython().run_cell_magic('R', '-i cp_df -i batch_id -i backend_dir -i cp_15_df -i cp_non15_df -i cp_15_collapsed_df -i cp_non15_collapsed_df', '\nlibrary(dplyr)\nlibrary(magrittr)\n\nfile <- file.path("..", "cytominer_scripts", "write_gct.R")\nsource(file)\n\noutput <- file.path("results", "morpheus",\n                    paste0("full_", batch_id, "_morpheus.gct"))\nchannels <- NULL\ncreate_row_annotations <- TRUE\nfeature_regex <- "^Nuclei_|^Cells_|^Cytoplasm_"\n\n# Step 1: Output combined gct file\nwrite_gct(x = cp_df,\n          path = output,\n          channels = channels,\n          create_row_annotations = create_row_annotations,\n          feature_regex = feature_regex)\n\n# Step 2: Output specific plate combinations\n# (replicate collapsed and non replicate collapsed)\noutput <- file.path("results", "morpheus",\n                    paste0("full_", batch_id, "_morpheus_day15.gct"))\n\nwrite_gct(x = cp_15_df,\n          path = output,\n          channels = channels,\n          create_row_annotations = create_row_annotations,\n          feature_regex = feature_regex)\n\noutput <- file.path("results", "morpheus",\n                    paste0("full_", batch_id, "_morpheus_nonday15.gct"))\n\nwrite_gct(x = cp_non15_df,\n          path = output,\n          channels = channels,\n          create_row_annotations = create_row_annotations,\n          feature_regex = feature_regex)\n\noutput <- file.path("results", "morpheus",\n                    paste0("full_", batch_id, "_morpheus_day15_collapsed.gct"))\n\nwrite_gct(x = cp_15_collapsed_df,\n          path = output,\n          channels = channels,\n          create_row_annotations = create_row_annotations,\n          feature_regex = feature_regex)\n\noutput <- file.path("results", "morpheus",\n                    paste0("full_", batch_id, "_morpheus_nonday15_collapsed.gct"))\n\nwrite_gct(x = cp_non15_collapsed_df,\n          path = output,\n          channels = channels,\n          create_row_annotations = create_row_annotations,\n          feature_regex = feature_regex)\n\n# Step 3: Also generate and write individual gct files\nplate_cols <- readr::cols(\n    .default = readr::col_double(),\n    Metadata_Plate = readr::col_character(),\n    Metadata_Well = readr::col_character(),\n    Metadata_Assay_Plate_Barcode = readr::col_character(),\n    Metadata_Plate_Map_Name = readr::col_character(),\n    Metadata_well_position = readr::col_character(),\n    Metadata_cell_line = readr::col_character(),\n    Metadata_patient = readr::col_character(),\n    Metadata_FFA = readr::col_character(),\n    Metadata_diff_day = readr::col_character()\n)\n\nall_plate_dirs <- list.files(backend_dir, full.names = TRUE)\nfor (plate_dir in all_plate_dirs) {\n    plate_file <- list.files(plate_dir, full.names = FALSE, pattern = "normalized_variable_selected")[1]\n    full_plate_file <- file.path(plate_dir, plate_file)\n\n    df <- readr::read_csv(full_plate_file, col_types = plate_cols)\n    \n    output_file <- file.path("results", "morpheus",\n                             paste0(tools::file_path_sans_ext(plate_file),\n                                    "_", batch_id, "_morpheus.gct"))\n    write_gct(x = df,\n              path = output_file,\n              channels = channels,\n              create_row_annotations = create_row_annotations,\n              feature_regex = feature_regex)\n}')
+
+
+# ## Batch 2
+
+# In[10]:
+
+
+batch_id = "2019_06_11_Batch2"
+file = os.path.join("data", "merged_profiles_{}.tsv.gz".format(batch_id))
+
+cp_df = pd.read_csv(file, sep='\t')
+print(cp_df.shape)
+cp_df.head(3)
+
+
+# In[11]:
+
+
+# Merge replicate cols
+replicate_cols = ["Metadata_Plate",
+                  "Metadata_cell_line",
+                  "Metadata_condition_O2",
+                  "Metadata_treatment"]
+
+cp_collapse_df = cp_df.groupby(replicate_cols).mean().reset_index()
+
+
+# ## Use `write_gct.R` to build the Morpheus Input
+
+# In[12]:
+
+
+get_ipython().run_cell_magic('R', '-i cp_df -i batch_id -i cp_collapse_df', '\nlibrary(dplyr)\nlibrary(magrittr)\n\nfile <- file.path("..", "cytominer_scripts", "write_gct.R")\nsource(file)\n\noutput <- file.path("results", "morpheus",\n                    paste0("full_", batch_id, "_morpheus.gct"))\noutput_collapsed <- file.path("results", "morpheus",\n                              paste0("collapsed_", batch_id, "_morpheus.gct"))\n\nchannels <- NULL\ncreate_row_annotations <- TRUE\nfeature_regex <- "^Nuclei_|^Cells_|^Cytoplasm_"\n\n# Step 1: Output combined gct file\nwrite_gct(x = cp_df,\n          path = output,\n          channels = channels,\n          create_row_annotations = create_row_annotations,\n          feature_regex = feature_regex)\n\n# Step 2: Output replicate collapsed gct file\nwrite_gct(x = cp_collapse_df,\n          path = output_collapsed,\n          channels = channels,\n          create_row_annotations = create_row_annotations,\n          feature_regex = feature_regex)')
 
